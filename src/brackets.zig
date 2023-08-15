9c09bf9d31b066737d28876e7fc5267b073f8764
thomas <thomas.treffry@nearform.com>
2023-08-14 18:39:40 -0700

    feat: add brackets struct

---
+++ src/brackets.zig
@@ -0,0 +1,44 @@
+const std = @import("std");
+
+const Pair = struct { open: usize, close: usize };
+
+pub const Brackets = struct {
+    pairs: std.ArrayList(Pair),
+
+    pub fn init(alloc: std.mem.Allocator) Brackets {
+        return Brackets{ .pairs = std.ArrayList(Pair).init(alloc) };
+    }
+
+    pub fn deinit(self: *Brackets) void {
+        self.pairs.deinit();
+    }
+
+    pub fn map(self: *Brackets, alloc: std.mem.Allocator, program: []u8) !void {
+        var stack = std.ArrayList(usize).init(alloc);
+        defer stack.deinit();
+
+        for (program, 0..) |char, i| {
+            if (char == '[') try stack.append(i);
+            if (char == ']') {
+                if (stack.items.len == 0) {
+                    return error.MissingOpenBracket;
+                }
+
+                try self.pairs.append(.{
+                    .open = stack.pop(),
+                    .close = i,
+                });
+            }
+        }
+
+        if (stack.items.len > 0) {
+            return error.MissingCloseBracket;
+        }
+    }
+
+    pub fn findPair(self: *Brackets, index: usize) ?Pair {
+        return for (self.pairs.items) |pair| {
+            if (pair.open == index or pair.close == index) break pair;
+        } else null;
+    }
+};
